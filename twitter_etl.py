092140effb7d46e3bd1edb850fc0b87613ef192d
sridharblue <sridharblue95@gmail.com>
2023-01-02 00:29:55 +0530

    init commit

---
+++ twitter_etl.py
@@ -0,0 +1,44 @@
+import tweepy
+import pandas as pd
+import os 
+import datetime
+
+def run_twitter_etl():
+
+    access_key = ""
+    access_secret = ""
+    consumer_key = ""
+    consumer_secret = ""
+
+
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)
+    auth.set_access_token(consumer_key, consumer_secret)
+
+    # Creating an API object
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name='@elonmusk',
+                            # 200 is the maximum allowed count
+                            count=200,
+                            include_rts = False,
+                            # Necessary to keep full_text
+                            # otherwise only the first 140 words are extracted
+                            tweet_mode = 'extended'
+                            )
+
+    list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {"user": tweet.user.screen_name,
+                        'text' : text,
+                        'favorite_count' : tweet.favorite_count,
+                        'retweet_count' : tweet.retweet_count,
+                        'created_at' : tweet.created_at}
+
+        list.append(refined_tweet)
+
+    df = pd.DataFrame(list)
+    pwd= os.getcwd()
+    os.makedirs(f"{pwd}/csv_files/" , exist_ok=True)
+    df.to_csv(os.path.join(f"{pwd}/csv_files/", f"{datetime.date.today()}_elon_twitter_data.csv"))
