6f6adceb654e38be54c558ad81d61b3bacb166a2
sridharblue <sridharblue95@gmail.com>
2023-01-04 12:14:39 +0530

    change csv interval

--- twitter_dag.py
+++ twitter_dag.py
@@ -12,7 +12,8 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=1)
+    'retry_delay': timedelta(minutes=1),
+    'schedule_interval': '@weekly',
 }
 
 
